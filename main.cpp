e85f9bcec0a8c756c67dce36d9d733937a3b8df7
deymon-d <d.scorpio2002@yandex.ru>
2022-05-22 02:00:10 +0300

    add function's map

--- main.cpp
+++ main.cpp
@@ -1,38 +1,60 @@
 #include "client.h"
 #include "symmetry_encryption.h"
 #include <iostream>
-
+#include <functional>
+#include <map>
 
 class Manager {
 public:
+    Manager() {
+        functions_["exit"] = &Manager::Exit;
+        functions_["send"] = &Manager::SendMessage;
+        functions_["get"] = &Manager::GetMessages;
+        functions_["update"] = &Manager::Update;
+    }
+
     void loop() {
         std::string command;
-        while (true) {
+        at_work_ = true;
+        while (at_work_) {
             client_.Prepare();
             std::cin >> command;
-            if (command == "exit") {
-                break;
-            } else if (command == "send") {
-                std::string name, text;
-                std::cin >> name >> text;
-                text = encryption(text);
-                std::cout << name << ' ' << text << std::endl;
-                client_.SendMessage(name, text);
-            } else if (command == "get") {
-                std::string name;
-                size_t count;
-                std::cin >> name >> count;
-                for (auto& message : client_.GetMessages(name, count)) {
-                    std::cout << decryption(message) << std::endl;
-                }
-            } else if (command == "update") {
-                client_.Update();
+            auto it = functions_.find(command);
+            if (it != functions_.end()) {
+                it->second(*this);
             }
         }
     }
 
 private:
     Client client_;
+    std::map<std::string, std::function<void(Manager&)>> functions_;
+    bool at_work_{false};
+
+    void Exit() {
+        at_work_ = false;
+    }
+
+    void SendMessage() {
+        std::string name, text;
+        std::cin >> name >> text;
+        text = encryption(text);
+        std::cout << name << ' ' << text << std::endl;
+        client_.SendMessage(name, text);
+    }
+
+    void GetMessages() {
+        std::string name;
+        size_t count;
+        std::cin >> name >> count;
+        for (auto& message : client_.GetMessages(name, count)) {
+            std::cout << decryption(message) << std::endl;
+        }
+    }
+
+    void Update() {
+        client_.Update();
+    }
 };
 
 int main() {
